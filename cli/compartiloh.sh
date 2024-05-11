6d31cd732b93f6a5026446f62b480fa2d30a7cf4
Martin Nicolas Perez <manperez@fi.uba.ar>
2024-05-11 19:53:18 -0300

    Add install.sh and CLI with initial x86_64 arch

---
+++ cli/compartiloh.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+INSTALATION_FOLDER=$(cd "$(dirname "$0")" && pwd)
+AGENT_FOLDER="$INSTALATION_FOLDER/agent/*/"
+GIT_FOLDER="$INSTALATION_FOLDER/git"
+
+# Función para conectar
+connecct() {
+    cd agent/*/
+    ./agent-client
+}
+
+# Función para actualizar
+update() {
+    cd $GIT_FOLDER
+    LAST_LOCAL_COMMIT=$(git log -1 --format="%H")
+    git fetch && LAST_REMOTE_COMMIT=$(git rev-parse origin/$(git rev-parse --abbrev-ref HEAD))
+    if [ "$LAST_LOCAL_COMMIT" = "$LAST_REMOTE_COMMIT" ]; then
+        echo "Cliente actualizado"
+    else
+        echo "Actualizando cliente"
+        git pull origin master
+        ACTUAL_DIST=$(basename "$AGENT_FOLDER")
+        DIST_VERSION="lib/$ACTUAL_DIST"
+        cp -p -r "$GIT_FOLDER/$DIST_VERSION" "$INSTALATION_FOLDER/agent"
+        echo "Cliente actualizado"
+    fi
+}
+
+# Función para mostrar la ayuda
+show_help() {
+    echo ""
+    echo "connect      Conectar el recurso al sistema. Requiere configuracion de token"
+    echo "update       Actualizar CLI de compartiloh"
+    echo "help         Mostrar este mensaje de ayuda"
+}
+
+# Verificar el número de argumentos
+if [ $# -eq 0 ]; then
+    echo "Se requiere al menos un parámetro."
+    show_help
+    exit 1
+fi
+
+# Manejar los parámetros
+case "$1" in
+    "connect")
+        echo "Conectando..."
+        connect
+        ;;
+    "update")
+        echo "Actualizando..."
+        update
+        ;;
+    "help")
+        show_help
+        ;;
+    *)
+        echo "Error: Parámetro no reconocido. Consulta la ayuda:"
+        show_help
+        ;;
+esac
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+#Validacion de entrada
+if [ $# -eq 0 ]; then
+    echo "Error: Se requiere al menos el parámetro de version a instalar"
+    exit 1
+fi
+
+#Configuracion de carpetas de instalacion
+DIST_VERSION="lib/$1"
+INSTALATION_FOLDER=/usr/local/compartiloh
+rm -rf $INSTALATION_FOLDER && mkdir -p $INSTALATION_FOLDER
+mkdir -p "$INSTALATION_FOLDER/agent"
+mkdir -p "$INSTALATION_FOLDER/git"
+
+#Instalando el cliente (accesible desde cualquier ruta)
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+cp -p -r "$SCRIPT_DIR/$DIST_VERSION" "$INSTALATION_FOLDER/agent"
+cp -p -r "$SCRIPT_DIR/cli/compartiloh.sh" "$INSTALATION_FOLDER/cloh"
+
+#Configuracion de repositorio
+REPO="https://github.com/compartiLOH/compartiloh-client"
+git clone $REPO "$INSTALATION_FOLDER/git"
